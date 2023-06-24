73ccee9b5d7baddbddf742e058e349a54a31d615
Shashank Gupta <shashankgupta9248@gmail.com>
2023-06-24 11:24:10 +0530

    changed navbar colour

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -36,7 +36,7 @@ function Navbar() {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ background: "lightgreen" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
